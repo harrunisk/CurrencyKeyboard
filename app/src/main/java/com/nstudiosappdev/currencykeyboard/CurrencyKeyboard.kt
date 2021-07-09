3f2e6718d6e358df0ffaf3b5958031edc92873a8
Harun <harrunisk@gmail.com>
2021-07-09 17:25:49 +0300

    Impl mutablestate flow

--- app/src/main/java/com/nstudiosappdev/currencykeyboard/CurrencyKeyboard.kt
+++ app/src/main/java/com/nstudiosappdev/currencykeyboard/CurrencyKeyboard.kt
@@ -1,7 +1,11 @@
 package com.nstudiosappdev.currencykeyboard
 
 import android.content.Context
+import android.graphics.Color
 import android.text.InputType
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.util.AttributeSet
 import android.view.*
 import android.view.inputmethod.EditorInfo
@@ -11,11 +15,9 @@ import com.google.android.material.button.MaterialButton
 import com.nstudiosappdev.currencykeyboard.databinding.LayoutCurrencyKeyboardBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import java.math.BigDecimal
 import java.text.NumberFormat
 import java.util.*
 
@@ -23,162 +25,147 @@ class CurrencyKeyboard @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr), View.OnClickListener {
 
-    private val _valueFlow: MutableStateFlow<Pair<Int, String>> =
+    private val _valueFlow: MutableStateFlow<Pair<Int, ArrayList<Char>>> =
         MutableStateFlow(Pair(VALUE_INITIAL_POSITION, VALUE_INITIAL))
-    val valueFlow: Flow<Pair<Int, String>> get() = _valueFlow
-
-    private val _decimalValueFlow: MutableStateFlow<Pair<Int, String>> =
-        MutableStateFlow(Pair(DECIMAL_VALUE_INITIAL_POSITION, DECIMAL_VALUE_INITIAL))
-    val decimalValueFlow: Flow<Pair<Int, String>> get() = _valueFlow
-
-    private val _decimalEnableFlow: MutableStateFlow<Boolean> = MutableStateFlow(false)
-    val decimalEnableFlow: Flow<Boolean> get() = _decimalEnableFlow
-
-    private val _buttonClickFlow: MutableSharedFlow<String> = MutableSharedFlow(1)
-    val buttonClickFlow: Flow<String> get() = _buttonClickFlow
-
-    private val _removeTextFlow: MutableStateFlow<Boolean> = MutableStateFlow(false)
-    val removeTextFlow: Flow<Boolean> get() = _removeTextFlow
+    private val valueFlow: Flow<Pair<Int, ArrayList<Char>>> get() = _valueFlow
 
     private lateinit var inputConnection: InputConnection
-    private var decimalEnabled: Boolean = false
-    private var decimalItemCount: Int = 0
+
+    val scope = Dispatchers.Main
+
+    var binding: LayoutCurrencyKeyboardBinding
 
     init {
+        binding =
+            LayoutCurrencyKeyboardBinding.inflate(
+                LayoutInflater.from(context), this, true
+            ).apply {
+                currencyKeyboard = this@CurrencyKeyboard
+                editText.setRawInputType(InputType.TYPE_CLASS_TEXT)
 
-    }
+                val ic = editText.onCreateInputConnection(EditorInfo())
+                ic?.let { setInputConnection(it) }
+                val numberFormatCurrencyInstance =
+                    NumberFormat.getCurrencyInstance(Locale("en", "AE"))
+                val currencyWithSpace = "${numberFormatCurrencyInstance.currency} "
 
-    suspend fun collectValues() {
-        combine(
-            valueFlow,
-            decimalValueFlow,
-            decimalEnableFlow,
-            buttonClickFlow,
-            removeTextFlow
-        ) { value, decimalValue, decimalEnabled, buttonClick, removeText ->
-
-            when (removeText) {
-                true -> {
-                    if (decimalEnabled) {
-                        val cursorPosition = decimalValue.first
-                        if (cursorPosition == 0) {
-                            _decimalEnableFlow.emit(false)
-                        } else {
-                            val newValue = decimalValue.second.toCharArray()
-                            newValue[cursorPosition] = '0'
-                            _removeTextFlow.value = false
-                            _decimalValueFlow.emit(Pair(cursorPosition - 1, newValue.toString()))
-                        }
-                    } else {
-                        val cursorPosition = value.first
-                        val newValue = value.second.substring(0, value.second.length - 1)
-                        _removeTextFlow.value = false
-                        _valueFlow.emit(Pair(cursorPosition - 1, newValue))
-                    }
-                }
-                false -> {
-                    when (decimalEnabled) {
-                        true -> {
-
-                        }
-                        false -> {
-
-                        }
-                    }
-                }
+                editText.hint =
+                    numberFormatCurrencyInstance.format(INITIAL_VALUE).replace(
+                        "${numberFormatCurrencyInstance.currency}", currencyWithSpace
+                    )
             }
-        }
-    }
 
-    private val binding: LayoutCurrencyKeyboardBinding =
-        LayoutCurrencyKeyboardBinding.inflate(
-            LayoutInflater.from(context), this, true
-        ).apply {
-            currencyKeyboard = this@CurrencyKeyboard
-            editText.setRawInputType(InputType.TYPE_CLASS_TEXT)
-
-            val ic = editText.onCreateInputConnection(EditorInfo())
-            ic?.let { setInputConnection(it) }
-            editText.addTextChangedListener(MoneyTextWatcher(editText))
-
-            val numberFormatCurrencyInstance = NumberFormat.getCurrencyInstance(Locale("en", "AE"))
-            val currencyWithSpace = "${numberFormatCurrencyInstance.currency} "
-
-            editText.hint =
-                numberFormatCurrencyInstance.format(INITIAL_VALUE).replace(
+        CoroutineScope(scope).launch {
+            valueFlow.collect {
+                val text = it.second.joinToString(separator = "")
+                val numberFormatCurrencyInstance =
+                    NumberFormat.getCurrencyInstance(Locale("en", "AE"))
+                val currencyWithSpace = "${numberFormatCurrencyInstance.currency} "
+                var cleanString = text.replace("[${currencyWithSpace}()+?$*,-]".toRegex(), "")
+                if (cleanString.isEmpty()) cleanString = "0"
+                val parsed = BigDecimal(cleanString)
+                val formatted: String = numberFormatCurrencyInstance.format(parsed).replace(
                     "${numberFormatCurrencyInstance.currency}", currencyWithSpace
                 )
+                val wordToSpan: Spannable = SpannableString(formatted)
+                val specialCharacterCount = formatted.count { it == ',' }
+                var spanPoint =
+                    currencyWithSpace.length + getCursorPosition() + specialCharacterCount
+                if (isEmptyState(getCursorPosition(), cleanString)) spanPoint = 0
 
+                setSpan(wordToSpan, spanPoint)
+                binding.editText.setText(wordToSpan)
+            }
         }
+    }
 
     override fun onClick(view: View?) {
-        when (view?.id) {
-            R.id.buttonDelete -> {
-                val text = (view as MaterialButton).text
-                CoroutineScope(Dispatchers.Main).launch {
-                    _removeTextFlow.emit(true)
-                }
-                if (decimalEnabled) {
-                    when (decimalItemCount) {
-                        DecimalItemCount.TWO_ITEM.itemCount -> {
-                            inputConnection.deleteSurroundingText(1, 0)
-                            decimalItemCount--
-                            inputConnection.commitText(SIGN_REMOVE_DECIMAL_ON_SECOND_PLACE, 3)
-                        }
-                        DecimalItemCount.ONE_ITEM.itemCount -> {
-                            inputConnection.deleteSurroundingText(1, 0)
-                            decimalItemCount--
-                            inputConnection.commitText(SIGN_REMOVE_DECIMAL_ON_FIRST_PLACE, 2)
-                        }
-                        else -> {
-                            decimalEnabled = false
-                            inputConnection.commitText(SIGN_DISABLE_DECIMAL, -1)
-                        }
-                    }
-                } else {
-                    inputConnection.deleteSurroundingText(1, 0)
-                }
-            }
-            R.id.buttonDot -> {
-                if (!decimalEnabled) {
-                    inputConnection.commitText(SIGN_ENABLE_DECIMAL, 2)
-                    decimalEnabled = true
-                }
-            }
-            else -> {
-                val text = (view as MaterialButton).text
-                if (decimalEnabled) {
-                    if (decimalItemCount < DecimalItemCount.TWO_ITEM.itemCount) {
-                        decimalItemCount++
-                        when (decimalItemCount) {
-                            DecimalItemCount.TWO_ITEM.itemCount -> {
-                                inputConnection.commitText(
-                                    "${text}${SIGN_DECIMAL_SECOND_PLACE_FILLED}",
-                                    1
-                                )
+        CoroutineScope(scope).launch {
+            when (view?.id) {
+                R.id.buttonDelete -> {
+                    if (getCursorPosition() != 0) {
+                        val cursorPosition = getCursorPosition()
+                        val currentText = getCurrentText()
+                        var newCursorPosition = cursorPosition
+                        when {
+                            cursorPosition == currentText.size - 2 -> {
+                                newCursorPosition--
                             }
-                            DecimalItemCount.ONE_ITEM.itemCount -> {
-                                inputConnection.commitText(
-                                    "${text}${SIGN_DECIMAL_FIRST_PLACE_FILLED}",
-                                    1
-                                )
+                            cursorPosition > currentText.size - 2 -> {
+                                currentText[cursorPosition - 1] = '0'
+                                newCursorPosition--
+                            }
+                            cursorPosition - 1 == 0 -> {
+                                currentText[cursorPosition - 1] = '0'
+                                newCursorPosition--
+                            }
+                            else -> {
+                                newCursorPosition--
+                                currentText.removeAt(newCursorPosition)
                             }
                         }
+                        _valueFlow.emit(Pair(newCursorPosition, currentText))
                     }
-                } else {
-                    inputConnection.commitText(text, 0)
+                }
+                R.id.buttonDot -> {
+                    var cursorPosition = getCursorPosition()
+                    val currentText = getCurrentText()
+                    if (cursorPosition == VALUE_INITIAL_POSITION) {
+                        cursorPosition += 2
+                    } else if (cursorPosition < currentText.size - 2) {
+                        cursorPosition++
+                    }
+                    _valueFlow.emit(Pair(cursorPosition, currentText))
+                }
+                else -> {
+                    if (getCurrentText().size < 10 || getCursorPosition() > (getCurrentText().size - 3)) {
+                        val text = (view as MaterialButton).text.first()
+                        val cursorPosition = getCursorPosition()
+                        val currentText = getCurrentText()
+                        var newCursorPosition = cursorPosition
+                        if (cursorPosition == 0 || cursorPosition == currentText.size - 2) {
+                            currentText[cursorPosition] = text
+                            if (view.text == INITIAL_VALUE.toString() && isEmptyState(cursorPosition, getCurrentText().joinToString(separator = ""))) newCursorPosition++
+                            newCursorPosition++
+                        } else if (cursorPosition == currentText.size - 1) {
+                            currentText[cursorPosition] = text
+                            newCursorPosition++
+                        } else if (cursorPosition == currentText.size) {
+
+                        } else {
+                            currentText.add(cursorPosition, text)
+                            newCursorPosition++
+                        }
+                        _valueFlow.emit(Pair(newCursorPosition, currentText))
+                    }
                 }
             }
         }
+    }
+
+    private fun getCursorPosition(): Int {
+        return _valueFlow.value.first
+    }
+
+    private fun getCurrentText(): ArrayList<Char> {
+        return _valueFlow.value.second
+    }
+
+    private fun isEmptyState(position: Int, text: String): Boolean {
+        return position == VALUE_INITIAL_POSITION && text == VALUE_INITIAL.joinToString(separator = "")
     }
 
     private fun setInputConnection(inputConnection: InputConnection) {
         this.inputConnection = inputConnection
     }
 
-    enum class DecimalItemCount(val itemCount: Int) {
-        TWO_ITEM(2),
-        ONE_ITEM(1)
+    private fun setSpan(spannable: Spannable, spanLength: Int) {
+        spannable.setSpan(
+            ForegroundColorSpan(Color.GRAY),
+            spanLength,
+            spannable.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
     }
 
     companion object {
@@ -194,10 +181,7 @@ class CurrencyKeyboard @JvmOverloads constructor(
         private const val INITIAL_VALUE = 0
 
         private const val VALUE_INITIAL_POSITION = 0
-        private const val VALUE_INITIAL = "0"
-
-        private const val DECIMAL_VALUE_INITIAL_POSITION = 0
-        private const val DECIMAL_VALUE_INITIAL = ".00"
+        private val VALUE_INITIAL = arrayListOf('0', '.', '0', '0')
 
     }
 }
